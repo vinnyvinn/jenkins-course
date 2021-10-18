ba2d7d1a5794de2a09ce941416a5e72069b80589
vinnyvinny <kituyiv@gmail.com>
2021-10-18 08:49:41 +0300

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -2,7 +2,7 @@ job('NodeJS example') {
     scm {
         git('git://github.com/vinnyvinn/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('kituyiv@gmail.com')
         }
     }
     triggers {
